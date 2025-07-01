219b279af1aa715cafbc5e3a1ce9fafc8168c0c1
Tyreek <tarek.sabbagh07@gmail.com>
2025-07-01 17:08:46 +0200

    create game logic

--- blackjack/game.py
+++ blackjack/game.py
@@ -13,5 +13,131 @@ class Deck:
     def shuffleDeck(self):
         random.shuffle(self.cards)
 
-    def drawCard(self):
+    def getCard(self):
         return self.cards.pop()
+    
+class Player:
+    def __init__(self):
+        self.hand = []
+
+    def drawCard(self, card):
+        self.hand.append(card)
+
+    def getHand(self):
+        return self.hand
+    
+    def resetHand(self):
+        self.hand = []
+
+    def getPoints(self):
+        points = 0
+        aces = 0
+
+        for point in self.hand:
+            if point == 11:
+                aces+=1
+                points += point
+            else:
+                points += point
+
+        while points > 21 and aces:
+            points -= 10
+            aces -= 1
+        
+        return points
+    
+    def hasBlackJack(self):
+        hasBlackJack = False
+        for card in self.hand:
+            if card == 11:
+                hasBlackJack = True
+        return hasBlackJack
+    
+    def isBusted(self):
+        points = self.getPoints()
+        if points > 21:
+            return True
+        else:
+            return False
+ 
+class Dealer(Player):
+    def __init__(self):
+        super().__init__()
+
+    def hasSoft17(self):
+        total = 0 
+        aces = 0
+        for card in self.hand:
+            if card == 11:
+                aces += 1
+                total +=11
+            else:
+                total += card
+
+        while total > 21 and aces:
+            total -=10
+            aces -=1
+        
+        if total == 17 and aces:
+            return True
+        else:
+            return False
+
+    
+    def playTurn(self, deck):
+        while True:
+            score = self.getPoints()
+
+            if score < 17 or (score == 17 and self.hasSoft17()):
+                self.drawCard(deck.getCard())
+            else:
+                break
+
+class BlackJackGame:
+    def __init__(self):
+        self.dealer = Dealer()
+        self.player = Player()
+        self.deck = Deck()
+        
+        self.deck.shuffleDeck()
+
+    def dealInitialCards(self):
+        self.player.drawCard(self.deck.getCard())
+        self.player.drawCard(self.deck.getCard())
+        self.dealer.drawCard(self.deck.getCard())
+
+    def playerHit(self):
+        self.player.drawCard(self.deck.getCard())
+
+    def playerStand(self):
+        self.dealer.playTurn(self.deck)
+
+    def getResult(self):
+        playerScore = self.player.getPoints()
+        dealerScore = self.player.getPoints()
+
+        if self.player.isBusted():
+            return -1
+        elif self.dealer.isBusted():
+            return 1
+
+        if playerScore > dealerScore:
+            return 1
+        elif playerScore < dealerScore:
+            return -1
+        else:
+            return 0
+
+    def resetGame(self):
+        self.player.resetHand()
+        self.dealer.resetHand()
+        self.deck = Deck()
+        self.deck.shuffleDeck()
+
+    def getState(self):
+        return {
+            "playerHand": self.player.getHand().copy(),
+            "dealerHand": self.dealer.getHand().copy(),
+            "playerPoints": self.player.getPoints(),
+            "dealerPoints": self.dealer.getPoints()
+        }
